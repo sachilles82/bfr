54f481e51decb024845ad33a1736a2bb62696f48
sachilles <kina98@gmx.ch>
2024-09-30 18:07:00 +0200

    Refactor DepartmentPolicy location and return values
    Moved DepartmentPolicy from app/Policies to app/Policies/BaseApp. Updated all policy methods to return true, reflecting default access permissions for all operations on the Department model.

--- app/Policies/BaseApp/DepartmentPolicy.php
+++ app/Policies/BaseApp/DepartmentPolicy.php
@@ -12,30 +12,37 @@ class DepartmentPolicy
 
     public function viewAny(User $user): bool
     {
+        return true;
 
     }
 
     public function view(User $user, Department $department): bool
     {
+        return true;
     }
 
     public function create(User $user): bool
     {
+        return true;
     }
 
     public function update(User $user, Department $department): bool
     {
+        return true;
     }
 
     public function delete(User $user, Department $department): bool
     {
+        return true;
     }
 
     public function restore(User $user, Department $department): bool
     {
+        return true;
     }
 
     public function forceDelete(User $user, Department $department): bool
     {
+        return true;
     }
 }
